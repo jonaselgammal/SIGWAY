7637acda590ae6eb469d90e81e63672481465447
Jonas El Gammal <jonas.el.gammal@rwth-aachen.de>
2026-06-24 11:30:51 +0200

    docs: theme + build config
    - mkdocstrings, mermaid, material icons, arithmatex/MathJax
    - drop mkdocs-jupyter (examples are markdown now); nav + Righteous headings

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,5 +1,16 @@
 window.MathJax = {
-  tex: { inlineMath: [["\\(","\\)"]], displayMath: [["\\[","\\]"]], processEscapes: true },
-  options: { ignoreHtmlClass: ".*", processHtmlClass: "arithmatex" }
+  tex: {
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*",
+    processHtmlClass: "arithmatex|jp-RenderedMarkdown"
+  }
 };
-document$.subscribe(() => { MathJax.startup.output.clearCache(); MathJax.typesetClear(); MathJax.typesetPromise(); });
+document$.subscribe(() => {
+  MathJax.typesetClear();
+  MathJax.typesetPromise();
+});
