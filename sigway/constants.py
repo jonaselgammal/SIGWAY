e5537e4905974adb9d1d332fcc9fa360de4c09c5
Mauropieroni <mauropieroni@gmail.com>
2026-07-01 16:18:50 +0200

    started to move constants around from utils to constants.py

---
+++ sigway/constants.py
@@ -0,0 +1,10 @@
+# Conversion factors
+c = 299792458.0  # Speed of light in m/s
+M_p = 2.176e-8  # Planck mass in kg (in natural units this would be 1)
+Mpc_to_m = 3.086e22  # 1 Mpc in meters
+CMB_scale = 0.05  # Mpc^-1
+CMB_scale_k = CMB_scale / Mpc_to_m * c  # Mpc^-1 to s^-1
+
+# Some cosmological parameters
+Omega_radiation_h2_today = 4.2e-5  # Omega_r h^2 today
+SM_CG_factor = 0.39  # CG factor for the SM
